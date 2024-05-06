b93797513d223394f7ac4bfc3d8e475f56475f39
Taissiralem <taissir.alem@gmail.com>
2024-05-06 19:05:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
 import Home from "./pages/Home"
-
+ // iframe at home.jsx 
 function App() {
 	return (
 		<div className="App">
